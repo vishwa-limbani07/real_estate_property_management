65fc3e9f12053a761cbdb5a10df81265f1d84186
Vishwa_limbani <limbanivishwa@gmail.com>
2024-09-20 17:20:03 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'real_estate_property_management';
+}
